c98f382bb45c9d8f8ad80e51a0c765037f75213f
mhutchinson <mhutchinson@gmail.com>
2019-09-08 12:25:11 +0100

    Limit parallelism of recursive impl
    This makes it about 600x faster for n=30.

--- fib/recursive.go
+++ fib/recursive.go
@@ -20,6 +20,14 @@ func ParallelRecursiveFib(n int) int {
 	if n < 0 {
 		return 0
 	}
+	const parallelDepth int = 5
+	return parallelRecursiveInternal(n, parallelDepth)
+}
+
+func parallelRecursiveInternal(n, p int) int {
+	if p < 0 {
+		return RecursiveFib(n)
+	}
 	if n < 2 {
 		return 1
 	}
@@ -28,11 +36,11 @@ func ParallelRecursiveFib(n int) int {
 	var a, b int
 	go func() {
 		defer wg.Done()
-		a = ParallelRecursiveFib(n - 1)
+		a = parallelRecursiveInternal(n-1, p-1)
 	}()
 	go func() {
 		defer wg.Done()
-		b = ParallelRecursiveFib(n - 2)
+		b = parallelRecursiveInternal(n-2, p-1)
 	}()
 	wg.Wait()
 	return a + b
