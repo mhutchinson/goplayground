4d7512f5a02ed94be3f999fb41c39efe5e79bdc2
mhutchinson <mhutchinson@gmail.com>
2019-09-08 15:29:28 +0100

    Added benchmark

--- hackerrank/hackerrank/projecteuler/001/main_test.go
+++ hackerrank/hackerrank/projecteuler/001/main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestMultiplesSum(t *testing.T) {
 	tables := []struct {
@@ -19,5 +21,11 @@ func TestMultiplesSum(t *testing.T) {
 		if got != c.want {
 			t.Errorf("MultiplesSum(%d): got %d, want %d", c.n, got, c.want)
 		}
+	}
+}
+
+func BenchmarkMultiplesSum(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		MultiplesSum(5000)
 	}
 }
