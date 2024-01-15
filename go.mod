9b4e468118cc923184dbe32ba44b6176108504d7
Martin Hutchinson <mhutchinson@gmail.com>
2024-01-15 16:26:41 +0000

    Added simple tool to print out the firmware types in AW log

--- go.mod
+++ go.mod
@@ -1,11 +1,16 @@
 module github.com/mhutchinson/goplayground
 
-go 1.17
+go 1.20
 
 require (
 	github.com/faiface/pixel v0.10.0
 	github.com/fogleman/gg v1.3.0
+	github.com/transparency-dev/serverless-log v0.0.0-20240115115102-149d17dc88b2
+	golang.org/x/mod v0.14.0
+	k8s.io/klog/v2 v2.120.0
 )
+
+require github.com/coreos/go-semver v0.3.1 // indirect
 
 require (
 	github.com/faiface/glhf v0.0.0-20181018222622-82a6317ac380 // indirect
@@ -13,7 +18,11 @@ require (
 	github.com/go-gl/gl v0.0.0-20190320180904-bf2b1f2f34d7 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20191125211704-12ad95a8df72 // indirect
 	github.com/go-gl/mathgl v0.0.0-20190416160123-c4601bc793c7 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
+	github.com/transparency-dev/armored-witness-common v0.0.0-20240111095920-905961d759e3
+	github.com/transparency-dev/formats v0.0.0-20230920083814-0f75b1d4e813 // indirect
+	github.com/transparency-dev/merkle v0.0.2 // indirect
 	golang.org/x/image v0.0.0-20190523035834-f03afa92d3ff // indirect
 )
---
+++ merkle/serverless.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/transparency-dev/armored-witness-common/release/firmware/ftlog"
+	"github.com/transparency-dev/serverless-log/client"
+	"golang.org/x/mod/sumdb/note"
+	"k8s.io/klog/v2"
+)
+
+var (
+	tlogURL = flag.String("url", "https://api.transparency.dev/armored-witness-firmware/ci/log/0/", "Base URL of a serverless log")
+	origin  = flag.String("origin", "transparency.dev/armored-witness/firmware_transparency/ci/0", "Origin string in checkpoint")
+	vkey    = flag.String("vkey", "transparency.dev-aw-ftlog-ci+f5479c1e+AR6gW0mycDtL17iM2uvQUThJsoiuSRirstEj9a5AdCCu", "Public key for log")
+)
+
+func main() {
+	flag.Parse()
+
+	ctx := context.Background()
+	f := func(ctx context.Context, path string) ([]byte, error) {
+		u, err := url.Parse(*tlogURL + path)
+		if err != nil {
+			klog.Exitf("Failed to parse URL: %v", err)
+		}
+		req, err := http.NewRequest("GET", u.String(), nil)
+		if err != nil {
+			return nil, err
+		}
+		resp, err := http.DefaultClient.Do(req.WithContext(ctx))
+		if err != nil {
+			return nil, err
+		}
+		switch resp.StatusCode {
+		case 404:
+			klog.Infof("Not found: %q", u.String())
+			return nil, os.ErrNotExist
+		case 200:
+			break
+		default:
+			return nil, fmt.Errorf("unexpected http status %q", resp.Status)
+		}
+		defer func() {
+			if err := resp.Body.Close(); err != nil {
+				klog.Errorf("resp.Body.Close(): %v", err)
+			}
+		}()
+		return io.ReadAll(resp.Body)
+	}
+	v, err := note.NewVerifier(*vkey)
+	if err != nil {
+		klog.Exitf("Failed to create verifier: %v", err)
+	}
+	cp, _, _, err := client.FetchCheckpoint(ctx, f, v, *origin)
+	if err != nil {
+		klog.Exitf("Failed to fetch checkpoint: %v", err)
+	}
+	klog.Infof("Got checkpoint for log of size %d", cp.Size)
+
+	for i := uint64(0); i < cp.Size; i++ {
+		leaf, err := client.GetLeaf(ctx, f, i)
+		if err != nil {
+			klog.Errorf("Failed to get leaf %d: %v", i, err)
+			continue
+		}
+		releaseNote, err := note.Open([]byte(leaf), note.VerifierList())
+		if err != nil {
+			if e, ok := err.(*note.UnverifiedNoteError); ok {
+				releaseNote = e.Note
+				klog.V(2).Infof("Note at leaf %d was not verified: %v", i, err)
+			} else {
+				klog.Errorf("Failed to open note at leaf %d: %v", i, err)
+				continue
+			}
+		}
+		var release ftlog.FirmwareRelease
+		if err := json.Unmarshal([]byte(releaseNote.Text), &release); err != nil {
+			klog.Errorf("Failed to unmarshal release at index %d: %v", i, err)
+		}
+		klog.Infof("Leaf %d: %s", i, release.Component)
+	}
+}
