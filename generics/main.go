23ae9cb5046288d5accccaf8edfce6927f122e5a
Martin Hutchinson <mhutchinson@gmail.com>
2024-05-29 19:47:37 +0100

    basic generic play

---
+++ generics/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(quickMaths(some(4)))
+	fmt.Println(quickMaths(none[int]()))
+}
+
+func quickMaths(v option[int]) int {
+	square := func(i int) int { return i * i }
+	add1 := func(i int) int { return i + 1 }
+	return v.bind(square).bind(add1).bind(square).get()
+}
+
+func some[T any](v T) option[T] {
+	return option[T]{
+		result: &v,
+	}
+}
+
+func none[T any]() option[T] {
+	return option[T]{
+		result: nil,
+	}
+}
+
+type option[T any] struct {
+	result *T
+}
+
+// bind applies the given function and returns another option,
+// taking care not to blow up if this option is none.
+// My original hope was to make this support taking a function
+// that took T and returned a new type, but go _methods_ do not
+// support generics!
+func (o option[T]) bind(f func(T) T) option[T] {
+	if o.result == nil {
+		return none[T]()
+	}
+	return some(f(o.get()))
+}
+
+func (o option[T]) get() T {
+	if o.result == nil {
+		var r T
+		return r
+	}
+	return *o.result
+}
